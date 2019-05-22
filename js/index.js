9ea3da87de0901eaf1232dcdc1697f956ab2b2be
Duy Dinh <ddinh8@ohlone.edu>
2019-05-22 09:45:27 -0700

    Updated the image height.

--- js/index.js
+++ js/index.js
@@ -26,35 +26,3 @@ $("body").click(function(e) {
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
 }
-
-
-jQuery('.carousel').carousel({interval: 3000 });
-// Normalize Carousel Heights - pass in Bootstrap Carousel items.
-function carouselNormalization() {
-var items = jQuery('#carouselExampleIndicators .carousel-item'), //grab all slides
-heights = [], //create empty array to store height values
-shortest; //create variable to make note of the shortest slide
-
-if (items.length) {
-function normalizeHeights() {
-items.each(function() { //add heights to array
-heights.push(jQuery(this).height());
-});
-shortest = Math.min.apply(null, heights); //cache largest value
-items.each(function() {
-jQuery('.carousel-item').css('height',shortest + 'px').css('overflow','hidden');
-
-});
-};
-normalizeHeights();
-
-jQuery(window).on('resize orientationchange', function () {
-shortest = 0, heights.length = 0; //reset vars
-items.each(function() {
-jQuery('.carousel-item').css('height','0'); //reset min-height
-});
-normalizeHeights(); //run it again
-});
-}
-}
-carouselNormalization();
